f6af8b44a074da2095a4aed5c0d1af1a8034552a
hell-noa <118801845+hell-noa@users.noreply.github.com>
2023-11-09 11:03:50 +0100

    Create client1.py

---
+++ TD/SOCKET/client1.py
@@ -0,0 +1,8 @@
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', port))
+server_socket.listen(1)
+conn, address = server_socket.accept()
+message = conn.recv(1024).decode()
+conn.send(reply.encode())
+conn.close()
+server_socket.close()

34b718483c3755326ee7fd00f45ed5e79a2c5821
hell-noa <118801845+hell-noa@users.noreply.github.com>
2023-11-10 11:22:30 +0100

    client1.py

--- TD/SOCKET/client1.py
+++ TD/SOCKET/client1.py
@@ -1,8 +1,33 @@
-server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', port))
-server_socket.listen(1)
-conn, address = server_socket.accept()
-message = conn.recv(1024).decode()
-conn.send(reply.encode())
-conn.close()
-server_socket.close()
+import socket
+import time
+host = "127.0.0.1"
+port = 1234
+flag= False
+
+client_socket = socket.socket()
+client_socket.connect((host,port))
+
+while flag == False:
+    message = input('message =' )
+    time.sleep(1)
+    try:     
+        client_socket.send(message.encode())
+        reply = client_socket.recv(1024).decode()
+        print(reply)
+            
+        if message == 'bye' or reply == "bye":
+            client_socket.close()
+            flag = True
+
+            
+            
+    except ConnectionAbortedError:
+        print ('la connexion a été coupé')
+    except ConnectionRefusedError:
+        print('la connexion a été  refusé')
+    except ConnectionResetError:
+        print('la connexion a été réinitialisé')
+        
+    else:
+        print('fin')
+        

b41e36d42895b515c962b65c8dd125207f7c93c6
hell-noa <118801845+hell-noa@users.noreply.github.com>
2023-11-10 18:25:48 +0100

    client1.py

--- TD/SOCKET/client1.py
+++ TD/SOCKET/client1.py
@@ -19,6 +19,10 @@ while flag == False:
             client_socket.close()
             flag = True
 
+        if message == 'arret' or reply == 'arret':
+            client_socket.close()
+            flag = True 
+
             
             
     except ConnectionAbortedError:
